44fa896ce74d6d48cecd03e221ac10ee8e0bd47f
prakhar <prakharagarwal1434@gmail.com>
2025-03-06 17:13:35 +0530

    utils added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 const app=express()
  
-app.use(cors())
+var corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+   
+  app.use(express.json({limit: "16kb"}))
+  app.use(express.urlencoded({extended: true, limit: "16kb"}))
+  app.use(express.static("public"))
+  app.use(cookieParser())
+  
+
 
 export {app}
