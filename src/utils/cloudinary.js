382af530985e979c04fd24339e50322aa821014f
prakhar <prakharagarwal1434@gmail.com>
2025-03-06 20:01:51 +0530

    configuration done

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from cloudinary
+import {v2 as cloudinary} from 'cloudinary'
 import fs from "fs"
 
 
@@ -7,3 +7,17 @@ cloudinary.config({
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET// Click 'View API Keys' above to copy your API secret
 });
+
+const cloudinary_uploader = async (filepath) => {
+    try {
+        if(!filepath) return null
+        const response = await cloudinary.uploader.upload(filepath,{
+            resource_type: "auto"
+        })
+        console.log("file uploaded successfully")
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filepath)//removes the locally saved file
+        return null;
+    }
+}
