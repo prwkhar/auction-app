2c5b1ece9be1ae1ceb1adf1b1406147ec18f6174
prakhar <prakharagarwal1434@gmail.com>
2025-03-13 16:22:14 +0530

    index with socket io

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
-// require('dotenv').config()
-import dotenv from "dotenv" 
-import mongoose from "mongoose";
+import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 import { app } from "./app.js";
-dotenv.config({
-    path: './env'
-})
+import http from "http";
+import { Server } from "socket.io";
 
-connectDb().then(()=>{
-    app.listen(process.env.PORT||8000,()=>{
-        console.log(`process is running at ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("MONGO db connection failed !!!",err);
-})
+dotenv.config({ path: "./env" });
 
+connectDb()
+  .then(() => {
+    const server = http.createServer(app);
+    const io = new Server(server, {
+      cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+      },
+    });
 
+    // Make Socket.IO instance available to controllers via app.locals
+    app.locals.io = io;
 
+    io.on("connection", (socket) => {
+      console.log(`Socket connected: ${socket.id}`);
 
+      // Allow clients to join specific auction rooms
+      socket.on("joinAuction", (auctionId) => {
+        socket.join(auctionId);
+        console.log(`Socket ${socket.id} joined auction ${auctionId}`);
+      });
 
+      socket.on("disconnect", () => {
+        console.log(`Socket disconnected: ${socket.id}`);
+      });
+    });
 
-
-
-// ;(async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGO_URI}`)
-//     } catch (error) {
-//         console.log("error :",error);
-//         throw error;
-//     }
-// })()
+    const PORT = process.env.PORT || 8000;
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed:", err);
+  });
