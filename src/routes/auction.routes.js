649bf9fba1b06c26aad3581bb83fc458c2f53bc6
prakhar <prakharagarwal1434@gmail.com>
2025-03-13 16:21:04 +0530

    auction routes

---
+++ src/routes/auction.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { hostAuction, getAuctions, getAuction, placeBid } from "../controllers/auction.controller.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+
+router.route("/host").post(verifyJWT, hostAuction);
+router.route("/").get(getAuctions);
+router.route("/:id").get(getAuction);
+router.route("/bid").post(verifyJWT, placeBid);
+
+export default router;
