9c6392fa939ff26f67dcabc30e480c3c857b839c
prakhar <prakharagarwal1434@gmail.com>
2025-03-13 16:21:38 +0530

    app.js added

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-const app=express()
- 
-var corsOptions = {
-    origin: process.env.CORS_ORIGIN,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-   
-  app.use(express.json({limit: "16kb"}))
-  app.use(express.urlencoded({extended: true, limit: "16kb"}))
-  app.use(express.static("public"))
-  app.use(cookieParser())
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
 
-  //routes importing
-import router from "./routes/user.routes.js"
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-//routes declaration
-app.use("/api/v1/users",router);//foruser
+import userRoutes from "./routes/user.routes.js";
+import auctionRoutes from "./routes/auction.routes.js";
 
-export {app}
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auctions", auctionRoutes);
+
+export { app };
