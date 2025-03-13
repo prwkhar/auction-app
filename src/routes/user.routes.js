a90c0372d3e5104b812ab98b26e8d0172f57d80b
prakhar <prakharagarwal1434@gmail.com>
2025-03-13 16:20:32 +0530

    user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { registerUser,loginUser } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.midlewares.js";
-import { verifyJWT } from "../middlewares/auth.midlewares.js";
-const router = Router()
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middlewares.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
