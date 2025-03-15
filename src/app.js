dd7635b17d907de2def34e920fdda8f34f025b6f
prakhar <prakharagarwal1434@gmail.com>
2025-03-15 11:49:10 +0530

    cors changed

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173", // Allow your frontend URL
-    credentials: true, // Allows cookies and auth headers
-  }));
+  origin: "https://auction-app-frontend-virid.vercel.app", // Allow entire frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
